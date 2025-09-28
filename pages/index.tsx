20c43ca58227c66f7f759951e30781bf80072e9c
GregKharlan <grigory.srtlyan@gmail.com>
2025-09-28 23:48:03 +0100

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import NavBr from '../components/NavBar';
+import NavBar from '../components/NavBar';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-    <title>HereWas - Home</title>
+        <title>HereWas - Home</title>
       </Head>
       <NavBar />
       <main className="container mx-auto p-4">
-        <h1 clasbName="b-2xl font-bold mb-4">Welcome to HereWas</h1>
+        <h1 className="text-2xl font-bold mb-4">Welcome to HereWas</h1>
         <p className="mb-4">Discover and share posts around you!</p>
       </main>
     </>
