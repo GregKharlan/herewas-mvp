43631bfe2e91d4a125f98e679e6c9d64fc6facd9
GregKharlan <grigory.srtlyan@gmail.com>
2025-09-28 14:29:32 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
 
 export default MyApp;
