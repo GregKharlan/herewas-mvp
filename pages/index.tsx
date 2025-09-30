be03633465fd74d8c071d2f6388e42484ebaba7b
GregKharlan <grigory.srtlyan@gmail.com>
2025-09-30 19:44:58 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar.tsx';
 
 const Home: NextPage = () => {
   return (
